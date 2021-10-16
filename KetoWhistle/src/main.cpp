0b82501822537cff8ff0c46ec0ff8d5a890c6713
Oliver <u6702233@anu.edu.au>
2021-10-16 16:53:16 +1100

    x

--- KetoWhistle/src/main.cpp
+++ KetoWhistle/src/main.cpp
@@ -72,6 +72,8 @@ void ketoWhistle_loop() {
   bool breath_detected = false;
   button_interrupt_flag = false; // Reset button interrupt.
   float acetone_level = 12.2;
+
+  baseline_co2 += co2_threshold;
   while (!breath_detected || !button_interrupt_flag) {
     breath_prompt(&display);
     
@@ -80,11 +82,12 @@ void ketoWhistle_loop() {
       //Serial.print(baseline_co2);
       //Serial.print("  ");
       //Serial.println(current_co2);
-      display_acetone_results(&display, 2000, baseline_co2 + co2_threshold);
+      display_acetone_results(&display, 2000, baseline_co2);
       current_co2 = measure_CO2(&scd30);
       display_acetone_results(&display, 2000, current_co2);
-      if (current_co2 > (baseline_co2 + co2_threshold)) {
+      if (current_co2 > (baseline_co2)) {
         breath_detected = true;
+        display_temperature(&display, 800, 50);
         break;
       }
       
@@ -94,13 +97,14 @@ void ketoWhistle_loop() {
       // delay(2000);
     }
 
-    if (current_co2 > (baseline_co2 + co2_threshold) || button_interrupt_flag) {
+    if (breath_detected || button_interrupt_flag) {
       acetone_level = measure_acetone(&pc_ads1220);
       acetone_level = 0.51;
-      breath_detected = true;
+      // breath_detected = true;
     } else {
       breath_abort_prompt(&display, 2000);
       baseline_co2 = min(current_co2, baseline_co2);
+      baseline_co2 += co2_threshold;
     }
     // acetone_level = measure_acetone(&pc_ads1220);
   }
